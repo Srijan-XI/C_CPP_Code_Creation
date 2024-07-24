a03397f1908f8c74a7bd16930ea8011a340d5730
Srijan Kumar <142709398+srijan1110@users.noreply.github.com>
2024-07-24 19:01:39 +0530

    find evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+ // program which use to see given number is even or odd
+#include<iostream>
+using namespace std;
+int main(){
+long long n;
+cout<<"Enter Number :";
+cin>>n;
+if(n%2==0){
+    cout<<"even number\n";
+  cout<<"yo";
+  }
+else {
+    cout<<"odd number\n";
+    cout<<"no";
+    }
+
+}
